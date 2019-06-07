f8b0f2614e45df15143172ee0414b7b7eb6eeea5
zhang <zh@anyline.org>
2019-06-07 08:46:21 +0800

    sql

--- anyboot-web/src/main/java/org/anyboot/dao/impl/springjdbc/AnybootDaoImpl.java
+++ anyboot-web/src/main/java/org/anyboot/dao/impl/springjdbc/AnybootDaoImpl.java
@@ -1,4 +1,7 @@
 package org.anyboot.dao.impl.springjdbc;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("anybootDao")
 public class AnybootDaoImpl extends org.anyline.dao.impl.springjdbc.AnylineDaoImpl{
 }
