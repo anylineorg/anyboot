008bf5b284f76cd37cb07ff473a9b470b819ea84
zhang <zh@anyline.org>
2019-08-27 11:19:57 +0800

    配置文件订制

--- anyboot-core/src/main/java/org/anyboot/util/ConfigTable.java
+++ anyboot-core/src/main/java/org/anyboot/util/ConfigTable.java
@@ -1,4 +1,19 @@
 package org.anyboot.util;
 
+
+import org.apache.log4j.Logger;
+
+
 public class ConfigTable extends org.anyline.util.ConfigTable{
+	private static final Logger log = Logger.getLogger(ConfigTable.class);
+	protected static final String version = "2.1.5.810";
+	protected static final String minVersion = "0007";
+	static{
+		init();
+		debug();
+	}
+
+	public static void init(){
+		init("anyboot");
+	}
 }
