b91e46465445893ea4bde2a230b70d6580414141
zhang <zh>
2019-12-03 21:36:41 +0800

    sql

--- anyboot-jdbc-mssql/src/main/java/org/anyboot/config/db/impl/mssql/SQLCreaterImpl.java
+++ anyboot-jdbc-mssql/src/main/java/org/anyboot/config/db/impl/mssql/SQLCreaterImpl.java
@@ -4,5 +4,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.msyql.creater")
 public class SQLCreaterImpl extends org.anyline.config.db.impl.mssql.SQLCreaterImpl{
-	private static final long serialVersionUID = 1L;
 }
