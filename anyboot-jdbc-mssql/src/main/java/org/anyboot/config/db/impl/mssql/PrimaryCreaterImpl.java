3f2ad8167af7a3fcb3ad5e2e4f15e0a0b83e2904
zhang <zh>
2019-12-03 17:58:49 +0800

    jdbc

---
+++ anyboot-jdbc-mssql/src/main/java/org/anyboot/config/db/impl/mssql/PrimaryCreaterImpl.java
@@ -0,0 +1,7 @@
+package org.anyboot.config.db.impl.mssql;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("mssql.primaryCreaterImpl")
+public class PrimaryCreaterImpl extends org.anyline.config.db.impl.mssql.PrimaryCreaterImpl{
+}
