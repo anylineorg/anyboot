02f04319975f13c2b38080057079916fb9973a8e
zhang <zh>
2019-12-03 17:58:49 +0800

    jdbc

---
+++ anyboot-jdbc-mysql/src/main/java/org/anyboot/config/db/impl/mysql/SQLCreaterImpl.java
@@ -0,0 +1,8 @@
+package org.anyboot.config.db.impl.mysql;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("anyboot.msyql.creater")
+public class SQLCreaterImpl extends org.anyline.config.db.impl.mysql.SQLCreaterImpl{
+	private static final long serialVersionUID = 1L;
+}
