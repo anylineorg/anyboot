bb3af5566e063d0c5661bc7dfafe8f617f6af846
zhang <zh>
2019-12-25 08:43:56 +0800

    8.3.7.111

--- anyboot-jdbc-oracle/src/main/java/org/anyboot/config/db/impl/oracle/SQLCreaterImpl.java
+++ anyboot-jdbc-oracle/src/main/java/org/anyboot/config/db/impl/oracle/SQLCreaterImpl.java
@@ -3,5 +3,5 @@ package org.anyboot.config.db.impl.oracle;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.jdbc.creater.oracle")
-public class SQLCreaterImpl extends org.anyline.config.db.impl.oracle.SQLCreaterImpl{
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.oracle.SQLCreaterImpl{
 }
