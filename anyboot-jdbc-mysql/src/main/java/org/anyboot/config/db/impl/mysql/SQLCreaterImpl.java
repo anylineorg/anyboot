ced2e7da4c5e4682d161e04c1e2dd4402c956ced
zhang <zh>
2019-12-25 08:43:55 +0800

    8.3.7.111

--- anyboot-jdbc-mysql/src/main/java/org/anyboot/config/db/impl/mysql/SQLCreaterImpl.java
+++ anyboot-jdbc-mysql/src/main/java/org/anyboot/config/db/impl/mysql/SQLCreaterImpl.java
@@ -3,5 +3,5 @@ package org.anyboot.config.db.impl.mysql;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.jdbc.creater.msyql")
-public class SQLCreaterImpl extends org.anyline.config.db.impl.mysql.SQLCreaterImpl{
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.mysql.SQLCreaterImpl{
 }
