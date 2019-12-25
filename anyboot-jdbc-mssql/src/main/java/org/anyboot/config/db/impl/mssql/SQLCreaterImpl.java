d0c2af589b08eb785704fb62ca09f2af53ee4257
zhang <zh>
2019-12-25 08:43:55 +0800

    8.3.7.111

--- anyboot-jdbc-mssql/src/main/java/org/anyboot/config/db/impl/mssql/SQLCreaterImpl.java
+++ anyboot-jdbc-mssql/src/main/java/org/anyboot/config/db/impl/mssql/SQLCreaterImpl.java
@@ -3,5 +3,5 @@ package org.anyboot.config.db.impl.mssql;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.jdbc.creater.mssql")
-public class SQLCreaterImpl extends org.anyline.config.db.impl.mssql.SQLCreaterImpl{
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.mssql.SQLCreaterImpl{
 }
