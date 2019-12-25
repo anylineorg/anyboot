cbca7a47a841cd571768b52b03e3e1791ac7a822
zhang <zh>
2019-12-25 08:43:56 +0800

    8.3.7.111

--- anyboot-jdbc-postgresql/src/main/java/org/anyboot/config/db/impl/postgresql/SQLCreaterImpl.java
+++ anyboot-jdbc-postgresql/src/main/java/org/anyboot/config/db/impl/postgresql/SQLCreaterImpl.java
@@ -3,5 +3,5 @@ package org.anyboot.config.db.impl.postgresql;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.jdbc.creater.postgresql")
-public class SQLCreaterImpl extends org.anyline.config.db.impl.postgresql.SQLCreaterImpl{
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.postgresql.SQLCreaterImpl{
 }
