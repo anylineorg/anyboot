7f95559e9fb57e2811a1abaf7d3e4a1b116f3fc3
zhang <zh>
2019-12-25 08:43:55 +0800

    8.3.7.111

--- anyboot-jdbc-hgdb/src/main/java/org/anyboot/config/db/impl/hgdb/SQLCreaterImpl.java
+++ anyboot-jdbc-hgdb/src/main/java/org/anyboot/config/db/impl/hgdb/SQLCreaterImpl.java
@@ -3,5 +3,5 @@ package org.anyboot.config.db.impl.hgdb;
 import org.springframework.stereotype.Repository;
 
 @Repository("anyboot.jdbc.creater.hgdb")
-public class SQLCreaterImpl extends org.anyline.config.db.impl.hgdb.SQLCreaterImpl{
+public class SQLCreaterImpl extends org.anyline.jdbc.config.db.impl.hgdb.SQLCreaterImpl{
 }
