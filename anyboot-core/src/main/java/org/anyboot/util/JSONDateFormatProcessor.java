e07278997bda59bd3093a1fefb2246837687f4a6
zhang <zh>
2020-01-02 16:11:10 +0800

    fast json

--- anyboot-core/src/main/java/org/anyboot/util/JSONDateFormatProcessor.java
+++
@@ -1,4 +0,0 @@
-package org.anyboot.util;
-
-public class JSONDateFormatProcessor extends org.anyline.util.JSONDateFormatProcessor{
-}
--- anyboot-weixin/src/main/java/org/anyboot/weixin/mp/tag/Pay.java
+++ anyboot-weixin/src/main/java/org/anyboot/weixin/mp/tag/Pay.java
@@ -1,4 +1,5 @@
 package org.anyboot.weixin.mp.tag;
 
 public class Pay extends org.anyline.weixin.mp.tag.Pay{
+	private static final long serialVersionUID = 4819483079442730548L;
 }
