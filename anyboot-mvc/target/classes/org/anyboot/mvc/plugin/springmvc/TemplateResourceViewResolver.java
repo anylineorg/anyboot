91b77f7e9505ce7c4297c62ec617164bdeaf123d
zhang <zh>
2020-01-08 22:24:50 +0800

    pom

---
+++ anyboot-mvc/target/classes/org/anyboot/mvc/plugin/springmvc/TemplateResourceViewResolver.java
@@ -0,0 +1,4 @@
+package org.anyboot.mvc.plugin.springmvc;
+
+public class TemplateResourceViewResolver extends org.anyline.plugin.springmvc.TemplateResourceViewResolver{
+}
---
+++ anyboot-mvc/target/classes/org/anyboot/mvc/plugin/springmvc/TemplateView.java
@@ -0,0 +1,4 @@
+package org.anyboot.mvc.plugin.springmvc;
+
+public class TemplateView extends org.anyline.plugin.springmvc.TemplateView{
+}
