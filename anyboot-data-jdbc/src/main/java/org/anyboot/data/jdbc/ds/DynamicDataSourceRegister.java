9316035b266ac037c47b876d5df8f63259cc1f77
anylineorg <server@anyline.org>
2023-01-06 17:15:03 +0800

    动态数据源注册

--- anyboot-data-jdbc/src/main/java/org/anyboot/data/jdbc/ds/DynamicDataSourceRegister.java
+++ anyboot-data-jdbc/src/main/java/org/anyboot/data/jdbc/ds/DynamicDataSourceRegister.java
@@ -13,11 +13,12 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.List;
 
-
+@Component
 public class DynamicDataSourceRegister implements ImportBeanDefinitionRegistrar, EnvironmentAware {
     private Logger log = LoggerFactory.getLogger(DynamicDataSourceRegister.class);
 
